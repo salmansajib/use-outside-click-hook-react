5e71315894d08729aa7ad004b2290e9c7e607490
Salman-sajib <sfsajib@gmail.com>
2024-04-28 14:52:47 +0600

    completed the test component

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,5 +1,29 @@
+import { useState } from 'react';
+
 function Test() {
-  return <div>Test</div>;
+  const [showContent, setShowContent] = useState(false);
+
+  return (
+    <div>
+      {showContent ? (
+        <div
+          style={{
+            backgroundColor: 'black',
+            padding: '1rem',
+            borderRadius: '5px',
+          }}
+        >
+          <h1>This is a random content</h1>
+          <p>
+            Please click outside ot this to close this. It won&apos;t close if
+            you click inside of this content.{' '}
+          </p>
+        </div>
+      ) : (
+        <button onClick={() => setShowContent(true)}>Show Content</button>
+      )}
+    </div>
+  );
 }
 
 export default Test;
