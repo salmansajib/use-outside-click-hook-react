5d0f1d8e921b9cc449eff3b6887384b0eaec83a4
Salman-sajib <sfsajib@gmail.com>
2024-04-28 15:17:47 +0600

    added all the functionalities

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,18 +1,35 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
+import UseOutsideClick from './UseOutsideClick';
 
 function Test() {
   const [showContent, setShowContent] = useState(false);
+  const ref = useRef();
+  UseOutsideClick(ref, () => setShowContent(false));
+
+  console.log(ref.current);
 
   return (
     <div>
       {showContent ? (
         <div
+          ref={ref}
           style={{
             backgroundColor: 'black',
-            padding: '1rem',
+            padding: '.1rem 1rem',
             borderRadius: '5px',
           }}
         >
+          <span
+            style={{
+              float: 'right',
+              fontSize: '1.5rem',
+              fontWeight: 'bold',
+              cursor: 'pointer',
+            }}
+            onClick={() => setShowContent(false)}
+          >
+            &times;
+          </span>
           <h1>This is a random content</h1>
           <p>
             Please click outside ot this to close this. It won&apos;t close if
